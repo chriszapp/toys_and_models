f9f6f24cf062918fc46ada4a6a12110cda3b38cf
bengubastug <115447066+BenguBastug@users.noreply.github.com>
2023-03-27 17:47:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,22 @@
 import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+!pip install mysqlclient
+import sqlalchemy as sql
+!pip install squarify
+import squarify
+connection = 'mysql://toyscie:WILD4Rdata!@51.178.25.157:23456/toys_and_models'
+sql_engine = sql.create_engine(connection)
+query_LQ1 = '''SELECT p.productName, sum(p.quantityInStock) as total_stock
+FROM orderdetails od
+    JOIN products p
+      ON od.productcode = p.productcode
+    GROUP BY p.productCode
+    ORDER BY sum(od.quantityOrdered) DESC
+    LIMIT 5;'''
+df_lq1 = pd.read_sql_query(query_LQ1, sql_engine)
+
+
 st.write('hello')
+df_lq1
